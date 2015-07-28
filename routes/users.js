a1a225836a937b6580d48258023c76d3a1ce48b8
Jack <kodeandplay@gmail.com>
2015-07-28 00:20:48 -0700

    Added comments to User model

--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,32 @@ var express = require('express'),
 	UserModel = require('../models/user');
 
 var router = express.Router({
-		caseSensitive: 	false,	// '/Foo' and '/foo' aret treated the same
-		mergeParams: 	false,	// Parent and child have conflicting param names
-		strict: 		false 	// '/foo' and '/foo/' are treated the same
-	});
+		
+	// '/foo' and '/foo/' are treated the same
+	strict: false, 	
+
+	// '/Foo' and '/foo' aret treated the same
+	caseSensitive: false,	
+
+	// Parent and child have conflicting param names
+	mergeParams: false
+
+});
 
 router.post('/', function(req, res, next) {
+	
+	// Create new user with data form post body
 	var user = new UserModel(req.body);
+	
 	user.save(function(err, result) {
+		
 		if(err) {
 			console.log('Error:', err);
 			return next(err);
 		}
 
 		res.send(result.toJSON());
+
 	});
 });
 
